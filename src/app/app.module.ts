554f08db15005c82ee1dec5e28280b5c44b3cc23
Marco Buschini <marco.buschini@gmail.com>
2025-05-01 12:45:40 +0200

    feat: complete update to anuglar 19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core'
 
 @NgModule({
-  declarations: [AppComponent],
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
