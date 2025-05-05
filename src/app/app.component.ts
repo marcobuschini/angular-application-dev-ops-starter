0b0fdc52050e40d113dbbbe2a61560c0fa0a0295
Marco Buschini <marco.buschini@gmail.com>
2025-05-05 15:01:28 +0200

    feat: update app to show some behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import {
+  MatSnackBar,
+  MatSnackBarConfig,
+  MatSnackBarModule,
+} from '@angular/material/snack-bar'
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
+  imports: [MatCardModule, MatButtonModule, MatSnackBarModule],
 })
 export class AppComponent {
   public title = 'frontend'
-  public constructor() {}
+  public constructor(public snackbar: MatSnackBar) {}
+
+  public ok(): void {
+    this.snackbar.open('You clicked Ok', null, {
+      duration: 1500,
+    } as MatSnackBarConfig)
+  }
+
+  public cancel(): void {
+    this.snackbar.open('You clicked Cancel', null, {
+      duration: 1500,
+    } as MatSnackBarConfig)
+  }
 }
